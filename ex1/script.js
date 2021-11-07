c3498ecf044d6ce6c1d66e7167742dbb858028c1
AbderrezakFel <73338588+AbderrezakFel@users.noreply.github.com>
2021-11-07 21:07:03 +0100

    Create script.js

---
+++ ex1/script.js
@@ -0,0 +1,21 @@
+   d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTM42Rbon532i3N3aW25ZpkoKtsMN-u6C7ycSj6A3DP2QCO89n3OMkseL--aI9a9s3ouwQJsVk6cOFw/pub?gid=513110937&single=true&output=csv", function (data) {
+            
+            for (i in data) {
+                if(data[i]["Dans quel parcours êtes vous inscris ?"]=="M2 THYP"){
+
+                    html = `
+                        <div class="col-6">
+                            <div class="p-3 border bg-light">
+                                <img src="`+data[i]["Votre photo"]+`">
+                                <h2>`+ data[i]["Votre nom"] +` `+ data[i]["Votre prénom"] +`</h2>
+                                <p>`+ data[i]["Votre mail"] +`</p>
+                                <p>`+ data[i]["N° étudiant"] +`</p>
+                                <p>`+ data[i]["Dans quel parcours êtes vous inscris ?"] +`</p>
+                            </div>
+                        </div>`;
+
+                    d3.select(".row").append("p").html(html);
+                }
+            }
+
+        });
