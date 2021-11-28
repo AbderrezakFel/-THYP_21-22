cbf417acf7e216ff5ac1077e5a3364ad68ee0292
AbderrezakFel <73338588+AbderrezakFel@users.noreply.github.com>
2021-11-28 22:08:00 +0100

    Create script.js

---
+++ omk/script.js
@@ -0,0 +1,18 @@
+        d3.json("data/resource_templates.json", function (data) {
+            
+            for (i in data) {
+
+                    html = `
+                    <div class="row gy-5">
+
+                        <div class="col-6">
+                            <div class="p-3 border bg-light">
+                                <p>`+ data[i]["o:id"] +` `+ data[i]["o:label"] +`</p>
+                            </div>
+                        </div>
+                    </div>`;
+
+                    d3.select(".container").append("p").html(html);
+                }
+
+        });
