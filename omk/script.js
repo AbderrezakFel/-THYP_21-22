cb156892a5c8231b55813ae78155d55a7680e719
AbderrezakFel <73338588+AbderrezakFel@users.noreply.github.com>
2021-11-29 08:24:11 +0100

    Update script.js

--- omk/script.js
+++ omk/script.js
@@ -11,3 +11,18 @@
                 }
 
         });
+
+
+d3.json("data/items.json", function (data) {
+            
+            for (i in data) {
+
+                    html = `<tr>
+                                <th scope="row">`+ data[i]["o:id"] +`</th>
+                                      <td>`+ data[i]["o:label"] +`</td>
+                             </tr>`;
+
+                    d3.select(".it").append("tr").html(html);
+                }
+
+        });
